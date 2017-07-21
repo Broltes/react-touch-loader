224c32d4591c7096711445f53f32c67e2e425719
Broltes <Broltes@Broltes-MacBook-Pro.local>
2017-07-21 11:46:36 +0800

    code formatting

--- demos/app.jsx
+++ demos/app.jsx
@@ -5,81 +5,81 @@ import './app.less';
 import Tloader from 'react-touch-loader';
 
 var App = React.createClass({
-    getInitialState: function() {
-        return {
-            canRefreshResolve: 1,
-            listLen: 0,
-            hasMore: 0,
-            initializing: 1,
-            refreshedAt: Date.now()
-        };
-    },
-    refresh: function(resolve, reject) {
-        setTimeout(() => {
-            if(!this.state.canRefreshResolve) return reject();
+  getInitialState: function () {
+    return {
+      canRefreshResolve: 1,
+      listLen: 0,
+      hasMore: 0,
+      initializing: 1,
+      refreshedAt: Date.now()
+    };
+  },
+  refresh: function (resolve, reject) {
+    setTimeout(() => {
+      if (!this.state.canRefreshResolve) return reject();
 
-            this.setState({
-                listLen: 9,
-                hasMore: 1,
-                refreshedAt: Date.now()
-            });
-            resolve();
-        }, 2e3);
-    },
-    loadMore: function(resolve){
-        setTimeout(() => {
-            var l = this.state.listLen + 9;
+      this.setState({
+        listLen: 9,
+        hasMore: 1,
+        refreshedAt: Date.now()
+      });
+      resolve();
+    }, 2e3);
+  },
+  loadMore: function (resolve) {
+    setTimeout(() => {
+      var l = this.state.listLen + 9;
 
-            this.setState({
-                listLen: l,
-                hasMore: l>0 && l<50
-            });
+      this.setState({
+        listLen: l,
+        hasMore: l > 0 && l < 50
+      });
 
-            resolve();
-        }, 2e3);
-    },
-    componentDidMount: function() {
-        setTimeout(() => {
-            this.setState({
-                listLen: 9,
-                hasMore: 1,
-                initializing: 2, // initialized
-            });
-        }, 2e3);
-    },
-    toggleCanReresh: function() {
-        this.setState({ canRefreshResolve: !this.state.canRefreshResolve});
-    },
+      resolve();
+    }, 2e3);
+  },
+  componentDidMount: function () {
+    setTimeout(() => {
+      this.setState({
+        listLen: 9,
+        hasMore: 1,
+        initializing: 2, // initialized
+      });
+    }, 2e3);
+  },
+  toggleCanRefresh: function () {
+    this.setState({ canRefreshResolve: !this.state.canRefreshResolve });
+  },
 
-    render: function(){
-        var { listLen, hasMore, initializing, refreshedAt, canRefreshResolve } = this.state;
-        var { refresh, loadMore, toggleCanReresh } = this;
-        var list = [];
+  render: function () {
+    var { listLen, hasMore, initializing, refreshedAt, canRefreshResolve } = this.state;
+    var { refresh, loadMore, toggleCanRefresh } = this;
+    var list = [];
 
-        if(listLen) {
-            for(var i = 0; i < listLen; i++){
-                list.push(
-                    <li key={i}>
-                        <p>{i}</p>
-                    </li>
-                );
-            }
-        }
-
-        return (
-            <div className="view">
-                <h1>react-touch-loader {refreshedAt.toString().substr(7)}</h1>
-
-                <Tloader className="main" onRefresh={refresh} onLoadMore={loadMore} hasMore={hasMore} initializing={initializing}>
-                    <ul>{list}</ul>
-                </Tloader>
-
-                <h2>
-                    <a href="https://github.com/Broltes/react-touch-loader">view source</a>
-                    <labe>can refresh resolve<input onChange={toggleCanReresh} type="checkbox" checked={canRefreshResolve}/></labe>
-                </h2>
-            </div>
+    if (listLen) {
+      for (var i = 0; i < listLen; i++) {
+        list.push(
+          <li key={i}>
+            <p>{i}</p>
+          </li>
         );
+      }
     }
+
+    return (
+      <div className="view">
+        <h1>react-touch-loader {refreshedAt.toString().substr(7)}</h1>
+
+        <Tloader className="main" onRefresh={refresh} onLoadMore={loadMore} hasMore={hasMore} initializing={initializing}>
+          <ul>{list}</ul>
+        </Tloader>
+
+        <h2>
+          <a href="https://github.com/Broltes/react-touch-loader">view source</a>
+          <labe>can refresh resolve<input onChange={toggleCanRefresh} type="checkbox" checked={canRefreshResolve} /></labe>
+        </h2>
+      </div>
+    );
+  }
 });
-render(<App/>, document.getElementById('app'));
+render(<App />, document.getElementById('app'));
--- src/react-touch-loader.jsx
+++ src/react-touch-loader.jsx
@@ -2,188 +2,188 @@ import React from 'react';
 import './touch-loader.less';
 
 const STATS = {
-    init: '',
-    pulling: 'pulling',
-    enough: 'pulling enough',
-    refreshing: 'refreshing',
-    refreshed: 'refreshed',
-    reset: 'reset',
+  init: '',
+  pulling: 'pulling',
+  enough: 'pulling enough',
+  refreshing: 'refreshing',
+  refreshed: 'refreshed',
+  reset: 'reset',
 
-    loading: 'loading'// loading more
+  loading: 'loading'// loading more
 };
 
 // pull to refresh
 // tap bottom to load more
 export default React.createClass({
-    getInitialState() {
-        return {
-            loaderState: STATS.init,
-            pullHeight: 0,
-            progressed: 0
-        };
-    },
-    getDefaultProps () {
-        return {
-            distanceToRefresh: 60,
-            autoLoadMore: 1
-        };
-    },
-    setInitialTouch(touch) {
-        this._initialTouch = {
-            clientY: touch.clientY
-        };
-    },
-    calculateDistance (touch) {
-        return touch.clientY - this._initialTouch.clientY;
-    },
-    // 拖拽的缓动公式 - easeOutSine
-    easing (distance) {
-        // t: current time, b: begInnIng value, c: change In value, d: duration
-        var t = distance;
-        var b = 0;
-        var d = screen.availHeight; // 允许拖拽的最大距离
-        var c = d / 2.5; // 提示标签最大有效拖拽距离
+  getInitialState() {
+    return {
+      loaderState: STATS.init,
+      pullHeight: 0,
+      progressed: 0
+    };
+  },
+  getDefaultProps() {
+    return {
+      distanceToRefresh: 60,
+      autoLoadMore: 1
+    };
+  },
+  setInitialTouch(touch) {
+    this._initialTouch = {
+      clientY: touch.clientY
+    };
+  },
+  calculateDistance(touch) {
+    return touch.clientY - this._initialTouch.clientY;
+  },
+  // 拖拽的缓动公式 - easeOutSine
+  easing(distance) {
+    // t: current time, b: begInnIng value, c: change In value, d: duration
+    var t = distance;
+    var b = 0;
+    var d = screen.availHeight; // 允许拖拽的最大距离
+    var c = d / 2.5; // 提示标签最大有效拖拽距离
 
-        return c * Math.sin(t / d * (Math.PI / 2)) + b;
-    },
-    canRefresh() {
-        return this.props.onRefresh && [STATS.refreshing, STATS.loading].indexOf(this.state.loaderState) < 0;
-    },
+    return c * Math.sin(t / d * (Math.PI / 2)) + b;
+  },
+  canRefresh() {
+    return this.props.onRefresh && [STATS.refreshing, STATS.loading].indexOf(this.state.loaderState) < 0;
+  },
 
-    touchStart(e) {
-        if(!this.canRefresh()) return;
-        if(e.touches.length == 1) this._initialTouch = {
-            clientY: e.touches[0].clientY,
-            scrollTop: this.refs.panel.scrollTop
-        };
-    },
-    touchMove(e) {
-        if(!this.canRefresh()) return;
-        var scrollTop = this.refs.panel.scrollTop;
-        var distance = this.calculateDistance(e.touches[0]);
+  touchStart(e) {
+    if (!this.canRefresh()) return;
+    if (e.touches.length == 1) this._initialTouch = {
+      clientY: e.touches[0].clientY,
+      scrollTop: this.refs.panel.scrollTop
+    };
+  },
+  touchMove(e) {
+    if (!this.canRefresh()) return;
+    var scrollTop = this.refs.panel.scrollTop;
+    var distance = this.calculateDistance(e.touches[0]);
 
-        if(distance > 0 && scrollTop <= 0){
-            var pullDistance = distance - this._initialTouch.scrollTop;
-            if(pullDistance < 0) {
-                // 修复webview滚动过程中touchstart时计算panel.scrollTop不准
-                pullDistance = 0;
-                this._initialTouch.scrollTop = distance;
-            }
-            var pullHeight = this.easing(pullDistance);
-            if(pullHeight) e.preventDefault();// 减弱滚动
+    if (distance > 0 && scrollTop <= 0) {
+      var pullDistance = distance - this._initialTouch.scrollTop;
+      if (pullDistance < 0) {
+        // 修复 webview 滚动过程中 touchstart 时计算panel.scrollTop不准
+        pullDistance = 0;
+        this._initialTouch.scrollTop = distance;
+      }
+      var pullHeight = this.easing(pullDistance);
+      if (pullHeight) e.preventDefault();// 减弱滚动
 
-            this.setState({
-                loaderState: pullHeight > this.props.distanceToRefresh ? STATS.enough : STATS.pulling,
-                pullHeight: pullHeight
-            });
-        }
-    },
-    touchEnd() {
-        if(!this.canRefresh()) return;
-        var endState = {
-            loaderState: STATS.reset,
-            pullHeight: 0
-        };
+      this.setState({
+        loaderState: pullHeight > this.props.distanceToRefresh ? STATS.enough : STATS.pulling,
+        pullHeight: pullHeight
+      });
+    }
+  },
+  touchEnd() {
+    if (!this.canRefresh()) return;
+    var endState = {
+      loaderState: STATS.reset,
+      pullHeight: 0
+    };
 
-        if (this.state.loaderState == STATS.enough) {
-            // refreshing
-            this.setState({
-                loaderState: STATS.refreshing,
-                pullHeight: 0
-            });
+    if (this.state.loaderState == STATS.enough) {
+      // refreshing
+      this.setState({
+        loaderState: STATS.refreshing,
+        pullHeight: 0
+      });
 
-            // trigger refresh action
-            this.props.onRefresh(() => {
-                // resove
-                this.setState({
-                    loaderState: STATS.refreshed,
-                    pullHeight: 0
-                });
-            }, () => {
-                // reject
-                this.setState(endState);// reset
-            });
-        }else this.setState(endState);// reset
-    },
-
-    loadMore(){
-        this.setState({ loaderState:  STATS.loading });
-        this.props.onLoadMore(() => {
-            // resolve
-            this.setState({loaderState: STATS.init});
+      // trigger refresh action
+      this.props.onRefresh(() => {
+        // resolve
+        this.setState({
+          loaderState: STATS.refreshed,
+          pullHeight: 0
         });
-    },
-    onScroll(e) {
-        if(
-            this.props.autoLoadMore &&
-            this.props.hasMore &&
-            this.state.loaderState != STATS.loading
-        ) {
-            let panel = e.currentTarget;
-            let scrollBottom = panel.scrollHeight - panel.clientHeight - panel.scrollTop;
+      }, () => {
+        // reject
+        this.setState(endState);// reset
+      });
+    } else this.setState(endState);// reset
+  },
 
-            if(scrollBottom < 5) this.loadMore();
-        }
-    },
+  loadMore() {
+    this.setState({ loaderState: STATS.loading });
+    this.props.onLoadMore(() => {
+      // resolve
+      this.setState({ loaderState: STATS.init });
+    });
+  },
+  onScroll(e) {
+    if (
+      this.props.autoLoadMore &&
+      this.props.hasMore &&
+      this.state.loaderState != STATS.loading
+    ) {
+      let panel = e.currentTarget;
+      let scrollBottom = panel.scrollHeight - panel.clientHeight - panel.scrollTop;
 
-    componentWillReceiveProps(nextProps) {
-        if(nextProps.initializing < 2) this.setState({
-            progressed: 0 // reset progress animation state
-        });
-    },
-    animationEnd(){
-        var newState = {};
+      if (scrollBottom < 5) this.loadMore();
+    }
+  },
 
-        if(this.state.loaderState == STATS.refreshed) newState.loaderState = STATS.init;
-        if(this.props.initializing > 1) newState.progressed = 1;
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.initializing < 2) this.setState({
+      progressed: 0 // reset progress animation state
+    });
+  },
+  animationEnd() {
+    var newState = {};
 
-        this.setState(newState);
-    },
-    render(){
-        const {
+    if (this.state.loaderState == STATS.refreshed) newState.loaderState = STATS.init;
+    if (this.props.initializing > 1) newState.progressed = 1;
+
+    this.setState(newState);
+  },
+  render() {
+    const {
             className,
-            hasMore,
-            initializing
+      hasMore,
+      initializing
         } = this.props;
-        const {
+    const {
             loaderState,
-            pullHeight,
-            progressed
+      pullHeight,
+      progressed
         } = this.state;
 
-        var footer = hasMore ? (
-            <div className="tloader-footer">
-                <div className="tloader-btn" onClick={this.loadMore}/>
-                <div className="tloader-loading"><i className="ui-loading"/></div>
-            </div>
-        ) : null;
+    var footer = hasMore ? (
+      <div className="tloader-footer">
+        <div className="tloader-btn" onClick={this.loadMore} />
+        <div className="tloader-loading"><i className="ui-loading" /></div>
+      </div>
+    ) : null;
 
-        var style = pullHeight ? {
-            WebkitTransform: `translate3d(0,${pullHeight}px,0)`
-        } : null;
+    var style = pullHeight ? {
+      WebkitTransform: `translate3d(0,${pullHeight}px,0)`
+    } : null;
 
-        var progressClassName = '';
-        if(!progressed){
-            if(initializing > 0) progressClassName += ' tloader-progress';
-            if(initializing > 1) progressClassName += ' ed';
-        }
-
-        return (
-            <div ref="panel"
-                className={`tloader state-${loaderState} ${className}${progressClassName}`}
-                onScroll={this.onScroll}
-                onTouchStart={this.touchStart}
-                onTouchMove={this.touchMove}
-                onTouchEnd={this.touchEnd}
-                onAnimationEnd={this.animationEnd}>
-
-                <div className="tloader-symbol">
-                    <div className="tloader-msg"><i/></div>
-                    <div className="tloader-loading"><i className="ui-loading"/></div>
-                </div>
-                <div className="tloader-body" style={style}>{this.props.children}</div>
-                {footer}
-            </div>
-        );
+    var progressClassName = '';
+    if (!progressed) {
+      if (initializing > 0) progressClassName += ' tloader-progress';
+      if (initializing > 1) progressClassName += ' ed';
     }
+
+    return (
+      <div ref="panel"
+        className={`tloader state-${loaderState} ${className}${progressClassName}`}
+        onScroll={this.onScroll}
+        onTouchStart={this.touchStart}
+        onTouchMove={this.touchMove}
+        onTouchEnd={this.touchEnd}
+        onAnimationEnd={this.animationEnd}>
+
+        <div className="tloader-symbol">
+          <div className="tloader-msg"><i /></div>
+          <div className="tloader-loading"><i className="ui-loading" /></div>
+        </div>
+        <div className="tloader-body" style={style}>{this.props.children}</div>
+        {footer}
+      </div>
+    );
+  }
 });
